feb42b300ff461652deadb7abdf45224a05f89b3
TusharSaini24 <sainitushar239@gmail.com>
2021-10-01 00:04:52 +0530

    added_module

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("uploading on git hub");
+// console.log("uploading on git hub");
+
+const http = require('http');
+const chalk = require('chalk');
+
+const server = http.createServer((req,res )=>{ 
+    console.log("server create ");
+    res.write('<h1> hello world</h1>');
+    console.log(chalk.blue('Hello world!'));
+    res.end();
+})
+
+server.listen('7000');
