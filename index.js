2a59b97d7c3ccdc6970c4868cd51011ad1e9d425
TusharSaini24 <sainitushar239@gmail.com>
2021-10-01 14:47:00 +0530

    file_system

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 const http = require('http');
 const chalk = require('chalk');
+const fs = require('fs');
 
 const server = http.createServer((req,res )=>{ 
     console.log("server create ");
-    res.write('<h1> hello world</h1>');
+    // res.write('<h1> hello world</h1>');
+    const htmlfile = fs.readFileSync('index.html');
+    res.write(htmlfile.toString());
     console.log(chalk.blue('Hello world!'));
     res.end();
 })
